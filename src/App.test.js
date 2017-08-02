1f80d995213166c4e63dc757e94acd80d34f618b
John Albritton <jma921@gmail.com>
2017-08-02 17:38:21 -0500

    Fix app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './components';
 
-jest.unmock('../config/constants');
+jest.unmock('./config/constants.js');
 
 it('renders without crashing', () => {
   shallow(<App />);
